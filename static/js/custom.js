e19ff652551bb2a0323aa2b898de609c6562ec53
visama396 <visama396@gmail.com>
2019-05-22 17:26:42 +0200

    Collected static

--- static/js/custom.js
+++ static/js/custom.js
@@ -14,20 +14,13 @@ $(document).ready(function() {
     // Cuando el scroll pase la seccion parallax, que el texto cambie de color para que se pueda ver.
     function cambiarNav() {
         if (document.body.scrollTop > $("#home").height()-50 || document.documentElement.scrollTop > $("#home").height()-50) {
-            if ($("#topnav").hasClass("navbar-dark")) {
-                $("#topnav").removeClass("navbar-dark");
-                $("#topnav").addClass("navbar-light");
-                $("#topnav").css("background-color", "#324c5f");
-            }
+            $("#topnav").removeClass('navbar-dark');
+            $("#topnav").addClass('navbar-light');
         } else {
-            if (!$("#topnav").hasClass("navbar-dark")) {
-                $("#topnav").removeClass("navbar-light");
-                $("#topnav").addClass("navbar-dark");
-                $("#topnav").css("background-color", "transparent");
-            }
+            $("#topnav").removeClass('navbar-light');
+            $("#topnav").addClass('navbar-dark');
         }
     }
     window.onscroll = cambiarNav;
     cambiarNav();
-    
 });
