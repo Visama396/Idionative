081cdcdb6d31def755aa98c0944a6d64f50f3b2d
Visama7799 <victor@stampwallet.com>
2019-05-22 10:58:58 +0200

    Fixing more files

--- idionative/apps.py
+++ idionative/apps.py
@@ -1,10 +1,4 @@
-<<<<<<< HEAD
 from django.apps import AppConfig
 
 class IdionativeConfig(AppConfig):
-=======
-from django.apps import AppConfig
-
-class IdionativeConfig(AppConfig):
->>>>>>> 084fda5ce9b0ca18567527acbf49540ba1aa0fdd
     name = 'idionative'
