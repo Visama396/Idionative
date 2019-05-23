080dda068d620757206ec7e3e0761c5917f55e78
Visama7799 <victor@stampwallet.com>
2019-05-23 15:38:45 +0200

    Homepage

--- idionative/views.py
+++ idionative/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 from .models import Course
 
 def home(request):
-    courses_list = Course.objects.get()
+    courses_list = Course.objects.get(lang__code_2__exact='en')
     return render(request, 'index.html')
 
 def home_files(request):
