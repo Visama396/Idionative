1d809f394faa8d6b9860adeb62fce82e52f003e6
Visama7799 <victor@stampwallet.com>
2019-05-24 12:02:13 +0200

    DRF

--- idionative/views.py
+++ idionative/views.py
@@ -1,7 +1,13 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from rest_framework import viewsets
 
-from .models import Course
+from .models import Course, Word
+from .serializers import WordSerializer
+
+class WordViewSet(viewsets.ModelViewSet):
+    queryset = Word.objects.all().order_by('name')
+    serializer_class = WordSerializer
 
 def home(request):
     courses_list = Course.objects.get(lang__code_2__exact='en')
