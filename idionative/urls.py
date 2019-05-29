b75c0acd2a8f170fc8e2bce012d681e6eee11dbb
Visama7799 <victor@stampwallet.com>
2019-05-29 09:08:25 +0200

    Courses url

--- idionative/urls.py
+++ idionative/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from django.conf.urls.i18n import i18n_patterns
 from rest_framework import routers
-from .views import home, home_files
+from .views import home, home_files, courses
 from rest_framework.urlpatterns import format_suffix_patterns
 from .views import WordList
 
@@ -13,5 +13,6 @@ urlpatterns = [
 urlpatterns += i18n_patterns(
     url(r'^$', home, name='home'),
     url(r'^words/$', WordList.as_view()),
+    url(r'^courses/$', courses, name='courses'),
     url(r'^admin/', admin.site.urls),
 )
