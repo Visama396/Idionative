d6135c9c4abce1f2c724ec11c54d71f64ffa0a66
Visama7799 <victor@stampwallet.com>
2019-05-29 08:50:03 +0200

    Fixing admin.py

--- idionative/admin.py
+++ idionative/admin.py
@@ -2,12 +2,12 @@ from django.contrib import admin
 
 # Register your models here
 
-from idionative.models import Language, Word, User, BookmarkedWords, Course, Unit, Page
+from idionative.models import Language, Word, User, BookmarkedWord, Course, Unit, Page
 
 admin.site.register(Language)
 admin.site.register(Word)
 admin.site.register(User)
-admin.site.register(BookmarkedWords)
+admin.site.register(BookmarkedWord)
 admin.site.register(Course)
 admin.site.register(Unit)
 admin.site.register(Page)
