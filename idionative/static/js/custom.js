c22aff74c0f2c24c7bf515c7e6720cc529df39a7
Visama7799 <victor@stampwallet.com>
2019-06-07 12:39:40 +0200

    Headline dutch

--- idionative/static/js/custom.js
+++ idionative/static/js/custom.js
@@ -6,23 +6,23 @@ $(document).ready(function() {
     lang = window.location.pathname.slice(1,3);
     var subtitle;
     if (lang === 'en') {
-        subtitle = new Typed("#subtitulo", {strings: ['^1000Learn a language,<br>^1000expand new horizons'], typeSpeed: 60});
+        subtitle = new Typed("#subtitulo", {strings: ['^1000learn a language,<br>^1000expand new horizons'], typeSpeed: 60});
     } else if (lang === 'es') {
-        subtitle = new Typed("#subtitulo", {strings: ['^1000Aprende un idioma,<br>^1000abre nuevos horizontes'], typeSpeed: 60});
+        subtitle = new Typed("#subtitulo", {strings: ['^1000aprende un idioma,<br>^1000abre nuevos horizontes'], typeSpeed: 60});
     } else if (lang === 'ja') {
         subtitle = new Typed("#subtitulo", {strings: ['^1000<span lang="ja">言語を学んで,<br>^1000新しいホライゾンを広げて</span>'], typeSpeed: 60});
     } else if (lang === 'de') {
-        subtitle = new Typed("#subtitulo", {strings: ['^1000Learn a language,<br>^1000expand new horizons'], typeSpeed: 60});
+        subtitle = new Typed("#subtitulo", {strings: ['^1000learn a language,<br>^1000expand new horizons'], typeSpeed: 60});
     } else if (lang === 'pt') {
-        subtitle = new Typed("#subtitulo", {strings: ['^1000Learn a language,<br>^1000expand new horizons'], typeSpeed: 60});
+        subtitle = new Typed("#subtitulo", {strings: ['^1000learn a language,<br>^1000expand new horizons'], typeSpeed: 60});
     } else if (lang === 'gl') {
-        subtitle = new Typed("#subtitulo", {strings: ['^1000Aprende a língua,<br>^1000expande novos horixontes'], typeSpeed: 60});
+        subtitle = new Typed("#subtitulo", {strings: ['^1000aprende a língua,<br>^1000expande novos horixontes'], typeSpeed: 60});
     } else if (lang === 'ko') {
         subtitle = new Typed("#subtitulo", {strings: ['^1000<span lang="ko">언어 배우기,<br>^1000새로운 지평을 넓히기</span>'], typeSpeed: 60});
     } else if (lang === 'vi') {
-        subtitle = new Typed("#subtitulo", {strings: ['^1000Ngôn ngữ mở ra<br>^1000chân trời mới'], typeSpeed: 60});
+        subtitle = new Typed("#subtitulo", {strings: ['^1000ngôn ngữ mở ra<br>^1000chân trời mới'], typeSpeed: 60});
     } else if (lang === 'nl') {
-        subtitle = new Typed("#subtitulo", {strings: ['^1000Learn a language,<br>^1000expand new horizons'], typeSpeed: 60});
+        subtitle = new Typed("#subtitulo", {strings: ['^1000leer een taal,<br>^1000en verrijk je horizon'], typeSpeed: 60});
     }
 
     $(".typed-cursor").hide();  // Oculta el cursor de la librería TypedJS
