c5b662ccd4ea6429875046a54453b8c947883323
Visama7799 <victor@stampwallet.com>
2019-09-09 16:55:49 +0200

    Fixed bug

--- idionative/static/js/stroke.js
+++ idionative/static/js/stroke.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
     $(".kana").mouseenter(function () {
-        $("#stroke-order").attr("src", "/static/assets/kana-strokes/" + this.text() + ".gif");
+        $("#stroke-order").attr("src", "/static/assets/kana-strokes/" + $(this).text() + ".gif");
     });
 });
